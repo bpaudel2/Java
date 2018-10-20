3f682ed12eabb05d1484ecb398b6916e1cabb60d
bpaudel2 <bpaudel2@uco.edu>
2018-10-20 14:07:59 -0500

    Update MyFirstJavaProgram.java

--- MyFirstJavaProgram.java
+++ MyFirstJavaProgram.java
@@ -2,9 +2,13 @@ public class MyFirstJavaProgram {
 
    /* This is my first java program.
     * This will print 'Hello World' as the output
+    * Some of things to be remember:
+    * The first letter of class name should always be Capital.
+    * Name of the program file should exactly match the class name.
+    * Java program processing starts from the main() method which is a mandatory part of every Java program.
     */
 
    public static void main(String []args) {
-      System.out.println("Hello World"); // prints Hello World
+      System.out.println("Hello Programmer"); // prints Hello World
    }
 }
